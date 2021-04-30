11a311b07cc5916181f6d7e7205a96a42ac4808a
Janos Tarjanyi <janos.tarjanyi@gmail.com>
2021-04-30 14:37:09 +0200

    base init

---
+++ ossiterm.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Convenience wrapper for running ossi_tool.__main__ directly from source tree."""
+
+
+from ossi_tool3.ossit import main
+
+
+if __name__ == '__main__':
+    main()
